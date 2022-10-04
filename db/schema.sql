e826be110c06b82b24213f69f11bcda91e3ec78e
Eric <e.kalin12@gmail.com>
2022-10-03 23:49:46 -0400

    updated tables and successfully finished ETL process

--- db/schema.sql
+++ db/schema.sql
@@ -1,61 +1,54 @@
-DROP DATABASE ProductOverview;
-CREATE DATABASE ProductOverview;
 
 CREATE TABLE product(
-  product_id PRIMARY KEY,
-  name VARCHAR(250) NOT NULL,
-  slogan VARCHAR(1000) NOT NULL,
-  description VARCHAR(1000) NOT NULL,
-  default_price INT NOT NULL,
-  category VARCHAR(250) NOT NULL
+  product_id SERIAL UNIQUE PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  slogan VARCHAR NOT NULL,
+  description VARCHAR NOT NULL,
+  category VARCHAR NOT NULL,
+  default_price INT NOT NULL
 )
 
--- -- intended to be join table
-CREATE TABLE product_feature(
-  product_feature_id PRIMARY KEY,
-  CONSTRAINT product_id FOREIGN KEY(product_id)
-  REFERENCES product(product_id),
-  feature_id INT NOT NULL
-)
+-- -- this table will be joined when querying instead
+-- CREATE TABLE product_feature(
+--   product_feature_id SERIAL UNIQUE PRIMARY KEY,
+--   product_id INT REFERENCES product(product_id),
+--   feature_id INT REFERENCES features(features_id)
+-- )
 
 CREATE TABLE features(
-  CONSTRAINT feature_id FOREIGN KEY(feature_id)
-  REFERENCES product_feature(feature_id),
-  name VARCHAR(250) NOT NULL,
-  value VARCHAR(250) NOT NULL
+  features_id SERIAL UNIQUE PRIMARY KEY,
+  product_id INT NOT NULL,
+  name VARCHAR NOT NULL,
+  value VARCHAR NOT NULL
 )
 
 CREATE TABLE product_style(
-  product_style_id PRIMARY KEY,
-  CONSTRAINT product_id FOREIGN KEY(product_id)
-  REFERENCES product(product_id),
-  name VARCHAR(250) NOT NULL,
+  product_style_id SERIAL UNIQUE PRIMARY KEY,
+  product_id INT REFERENCES product(product_id),
+  name VARCHAR NOT NULL,
+  sale_price TEXT,
   original_price INT NOT NULL,
-  sale_price INT NOT NULL,
-  default BOOLEAN NOT NULL
+  "default" BOOLEAN NOT NULL
 )
 
 CREATE TABLE sku(
-  sku_id PRIMARY KEY,
-  CONSTRAINT style_id FOREIGN KEY(product_style_id)
-  REFERENCES product_style(product_style_id),
-  quantity INT NOT NULL,
-  size VARCHAR(15) NOT NULL
-
+  sku_id SERIAL UNIQUE PRIMARY KEY,
+  product_style_id INT REFERENCES product_style(product_style_id),
+  size VARCHAR NOT NULL,
+  quantity INT NOT NULL
 )
 
 CREATE TABLE photo(
-  photo_id PRIMARY KEY,
-  CONSTRAINT style_id FOREIGN KEY(style_id)
-  REFERENCES style(style_id),
-  thumbnail_url VARCHAR(500) NOT NULL,
-  url VARCHAR(500) NOT NULL
+  photo_id SERIAL UNIQUE PRIMARY KEY,
+  product_style_id INT REFERENCES product_style(product_style_id),
+  thumbnail_url VARCHAR NOT NULL,
+  url VARCHAR NOT NULL
 )
 
-CREATE TABLE related(
-  related_id PRIMARY KEY,
-  CONSTRAINT related_product_id FOREIGN KEY(product_id)
-  REFERENCES product(product_id),
-  CONSTRAINT product_id FOREIGN KEY(product_id)
-  REFERENCES product(product_id)
-)
+-- This table will be joined when querying
+-- CREATE TABLE related(
+--   related_id SERIAL UNIQUE PRIMARY KEY,
+--   CONSTRAINT related_product_id FOREIGN KEY(product_id)
+--   REFERENCES product(product_id),
+--   product_id INT REFERENCES product(product_id)
+-- )
