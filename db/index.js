093f87b1cfdfaff41902bb0d8cf2fdbe7ae365e2
Eric <e.kalin12@gmail.com>
2022-10-05 18:34:15 -0400

    Successfully created manual connection to db and can query tables

--- db/index.js
+++ db/index.js
@@ -1,3 +1,18 @@
+require('dotenv').config();
 const postgres = require('postgres');
 
 const sql = postgres('postgres://eric@localhost:5432/ProductOverview')
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  "max": 20,
+  "connectionTimeoutMillis": 0,
+  "idleTimeoutMillis": 0
+})
+
+module.exports = {
+  pool,
+  query: (text, params) => {
+    return pool.query(text, params)
+  },
+}
--- server/server.js
+++ server/server.js
@@ -1,4 +1,27 @@
-const app = require("./app");
+const app = require('./app');
+const { pool, query } = require('../db/index.js')
+
+app.get('/products', (req, res) => {
+
+  pool
+  .query('SELECT * FROM product')
+  .then(res => console.log('results: ', res.rows))
+  .catch(err => console.log('error: ', err))
+
+})
+
+app.get('/products/:product_id', (req, res) => {
+
+})
+
+app.get('/products/:product_id/styles', (req, res) => {
+
+})
+
+app.get('/products/:product_id/related', (req, res) => {
+
+})
+
 
 app.listen(3000, () => {
   console.log("listening on port 3000!");
