a2b9b0375a87b99543cc89c52cb7a2fb760c71d0
Eric <e.kalin12@gmail.com>
2022-10-12 23:09:10 -0400

    successfully finished general shape of product styles obj

--- server/server.js
+++ server/server.js
@@ -11,6 +11,19 @@ app.get('/products', (req, res) => {
 })
 
 app.get('/products/:product_id', (req, res) => {
+  let req_id = req.params.product_id;
+  let res_obj;
+  pool
+  // .query(`SELECT * FROM product INNER JOIN features ON product.product_id = features.product_id WHERE product.product_id = '${req_id}'`)
+  .query(`SELECT * FROM product WHERE product_id = '${req_id}'`)
+  .then(res => res_obj = res.rows[0])
+  .catch(err => console.log('error: ', err))
+
+  pool
+  .query(`SELECT name,value FROM features WHERE product_id = '${req_id}'`)
+  .then(res => res_obj.features = res.rows)
+  .then(res => console.log('new obj shape: ', res_obj))
+  .catch(err => console.log('error: ', err))
 
 })
 
