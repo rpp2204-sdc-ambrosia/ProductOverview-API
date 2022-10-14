e41e7bf66ce0c003e1fac31c9a446c687cdb4677
Eric <e.kalin12@gmail.com>
2022-10-13 23:22:57 -0400

    successfully completed product styles response shape, despite empty object bug

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
 app.get("/", (req, res) => {
   res.status(200).send("Hello World!");
 });
--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const app = require('./app');
 const { pool, query } = require('../db/index.js')
 
 app.get('/products', (req, res) => {
-
   pool
   .query('SELECT * FROM product')
   .then(res => console.log('results: ', res.rows))
@@ -11,7 +10,7 @@ app.get('/products', (req, res) => {
 })
 
 app.get('/products/:product_id', (req, res) => {
-  let req_id = req.params.product_id;
+  const req_id = req.params.product_id;
   let res_obj;
   pool
   // .query(`SELECT * FROM product INNER JOIN features ON product.product_id = features.product_id WHERE product.product_id = '${req_id}'`)
@@ -28,6 +27,51 @@ app.get('/products/:product_id', (req, res) => {
 })
 
 app.get('/products/:product_id/styles', (req, res) => {
+  const req_id = req.params.product_id
+  console.log('req_id: ', req_id)
+  let res_obj = {
+    product_id: req_id
+  };
+
+  pool
+  .query(`SELECT product_style_id,name,original_price,sale_price,"default" FROM product_style WHERE product_id = '${req_id}' ORDER BY product_id ASC`)
+  .then((data) => {
+
+    let resultArr = data.rows.map((obj) => {
+      //console.log('inner map: ', obj)
+      return pool
+      .query(`SELECT * FROM photo WHERE product_style_id = '${obj.product_style_id}'`)
+      .then((data) => {
+        console.log('photo results: ', data.rows)
+        obj.photos = data.rows
+      })
+      .then((data) => {
+        return pool.query(`SELECT quantity,size FROM sku WHERE product_style_id = '${obj.product_style_id}'`)
+      })
+      .then((data) => {
+        console.log('sku results: ', data.rows)
+        obj.skus = data.rows
+      })
+      .then(data => resultArr.push(obj))
+      .then(data  => res_obj.results = resultArr)
+      .then((data) => {
+        // console.log('Inner res_obj: ', res_obj)
+        return res_obj;
+      })
+      .catch(err => console.log('error: ', err))
+
+    })
+    Promise.all(resultArr)
+    .then(data => res.send(res_obj))
+    .catch(err => console.log('error: ', err))
+
+  })
+
+  // try {
+  //   const ps_query = await pool.query(`SELECT product_style_id,name,original_price,sale_price,"default" FROM product_style WHERE product_id = '${req_id}' ORDER BY product_id ASC`)
+
+  // }
+
 
 })
 
