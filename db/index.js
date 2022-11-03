db88c3481a95c80c9596869d47c78a723e4f84ff
Eric <e.kalin12@gmail.com>
2022-11-03 18:00:30 -0400

    forgot to include .env file within gitignore

--- db/index.js
+++ db/index.js
@@ -1,13 +1,19 @@
 require('dotenv').config();
-const postgres = require('postgres');
+// const postgres = require('postgres');
 
-const sql = postgres('postgres://eric@localhost:5432/ProductOverview')
+// const sql = postgres('postgres://eric@localhost:5432/ProductOverview')
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
   "max": 20,
   "connectionTimeoutMillis": 0,
-  "idleTimeoutMillis": 0
+  "idleTimeoutMillis": 0,
+  user: 'postgres',
+  host: '52.91.86.207',
+  database: 'productoverview',
+  password: 'student',
+  // port: 5432
 })
 
 module.exports = {
