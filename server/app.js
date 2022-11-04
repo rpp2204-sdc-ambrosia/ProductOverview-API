a02cfceca83034afbaa1c537e3351bf3b1196afd
Eric <e.kalin12@gmail.com>
2022-11-04 12:08:47 -0400

    Added new route to stress test with loader.io

--- server/app.js
+++ server/app.js
@@ -1,3 +1,5 @@
+// loaderio-7e85e7d29e5a01f1b2a1d014c29ec27a
+
 const express = require("express");
 const router = require("../server/server.js");
 const app = express();
@@ -8,6 +10,11 @@ app.use(router)
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello World!");
+});
+
+app.get("/loaderio-7e85e7d29e5a01f1b2a1d014c29ec27a.txt", (req, res) => {
+  // console.log('result: ', `${__dirname}/loaderio-7e85e7d29e5a01f1b2a1d014c29ec27a.txt`)
+  res.sendFile(`${__dirname}/loaderio-7e85e7d29e5a01f1b2a1d014c29ec27a.txt`);
 });
 
 app.listen(3000, () => {
